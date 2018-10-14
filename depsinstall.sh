71ae9147681625784fb17e0992b378c2e467f5e2
Josh Hawkins <hawkins@users.noreply.github.com>
2018-10-14 00:48:02 -0400

    Initial depsinstall script version

---
+++ depsinstall.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# ZSH
+# TODO: Test this
+sudo apt-get install zsh
+chsh $(whoami) /bin/zsh
+
+# Oh-My-ZSH!
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Zeit Oh-My-ZSH theme
+curl https://raw.githubusercontent.com/zeit/zeit.zsh-theme/master/zeit.zsh-theme -Lo ~/.oh-my-zsh/themes/zeit.zsh-theme
+
+# Hub
+which brew && brew install hub
+# TODO: Windows, linux
+
+# rbenv
+which brew && brew install rbenv
+# TODO: Linux, Windows install of rbenv
+
+# Node
+# TODO: Determine platform and run appropriate script
+echo "Go install Node.js from their website, or via package managers: https://nodejs.org/en/download/package-manager/"
+
+
