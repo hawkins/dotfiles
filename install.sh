a48dc3275be11dc1f320a5360430417ed60691d7
Josh Hawkins <hawkins@users.noreply.github.com>
2018-10-19 18:59:02 -0400

    Add zsh syntax highlighting
    Not sold on this, but, trying it out

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ echo "# Run dotfiles/install.sh to repair this.     #" >> ~/.dotfiles_config
 echo "###############################################" >> ~/.dotfiles_config
 echo "export DOTFILES=$DOTFILES" >> ~/.dotfiles_config
 
-# List all files and folders to be symlinked
-files=($(find . -name '*.symlink' -print | sed s/\.\.//))
+# Handle submodules before we do anything else
+git submodule update --init --recursive
 
 # Link $1 to ~/.$2
 # Notice this adds the "." prefix
@@ -17,21 +17,22 @@ function link() {
   ln -sfn $DOTFILES/$1 ~/.$2 && echo "Linked $1 to ~/.$2"
 }
 
-function parse-filename() {
+# Reformat name from its friendly name
+function strip() {
   local file=$(basename $1)
   local file="${file#.}"
   local file="${file%.symlink}"
   echo $file
 }
 
+# Symlink all files and folders whose name ends with `.symlink`
+files=($(find . -name '*.symlink' -print | sed s/\.\.//))
 for i in "${files[@]}"
 do
-  :
-  file=$(parse-filename $i)
-  link $i $file
+  link $i $(strip $i)
 done
 
-# Special purpose files
+# Link special purpose files
 ln -fn $DOTFILES/zsh/zeit.zsh-theme ~/.oh-my-zsh/themes/zeit.zsh-theme && echo "Installed ZSH Theme: Zeit"
 
 # Finally, prepare the shell with new dotfiles
